7cf4fe2c4cf805266d40c01f1b40689602e1ef1f
Paty <paty.amaya119@gmail.com>
2021-12-15 16:38:37 -0600

    Correcciones en RecordsScreen

--- ProyectoGreenfoot/RecordsScreen.java
+++ ProyectoGreenfoot/RecordsScreen.java
@@ -2,6 +2,8 @@ import greenfoot.*;
 import java.io.*;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.List;
 
 public class RecordsScreen extends World
 {
@@ -13,6 +15,23 @@ public class RecordsScreen extends World
     }
 
     public void showScore(){
+        int y = 150;
+        RecordManager recordManager = new RecordManager(5, "records.txt");
+        List<Record> records = recordManager.getRecords();
+        
+        for(Record record : records){
+            
+            GreenfootImage showText = getBackground();
+            Font adjustedFont = new Font(null,true, false, 30);
+            showText.setColor(Color.BLACK);
+            showText.setFont(adjustedFont);
+            showText.drawString(record.getPlayer()+" - "+record.getScore(),140,y);
+            y+=52;
+        }
+    }
+    
+    /*
+    public void showScore(){
         int y = 100;
         String nameAndScore;
 
@@ -20,7 +39,7 @@ public class RecordsScreen extends World
             File file = new File("records.txt");    
             Scanner entry = new Scanner(new FileReader(file));
 
-            do{
+            while(entry.hasNext()){
                 nameAndScore = entry.next();
                 GreenfootImage showText = getBackground();
                 Font adjustedFont = new Font(null,true, false, 30);
@@ -28,12 +47,11 @@ public class RecordsScreen extends World
                 showText.setColor(Color.BLACK);
                 showText.setFont(adjustedFont);
                 showText.drawString(nameAndScore, 140, y+=52);
-            }while(entry.hasNext());
+            }
         }catch(IOException e){
 
         }
-    }
-    
+    }*/
     private void prepare()
     {
         HomeButton homeButton = new HomeButton();
